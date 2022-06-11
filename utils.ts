bc344036de91447712a24d0e0f70ce78fc840a6b
Alex Chornyi <chornyi.oleksandr@lll.kpi.ua>
2022-06-11 14:48:45 +0300

    Create binaryScheduleToTasks adapter and some helping utils

--- utils.ts
+++ utils.ts
@@ -1,4 +1,5 @@
-import { BinarySchedule, Task } from "./models/Task.model";
+import { identity } from "ramda";
+import { BinarySchedule, Executor, Task } from "./models/Task.model";
 
 export function isOverlap(schedule: Task[], task: Task): boolean {
   return schedule.reduce(
@@ -23,6 +24,36 @@ export const tasksToBinarySchedule: TasksToBinarySchedule = (
     tasks.some((task) => task.executor === executor)
   );
 };
+
+export interface BinaryScheduleToTasks {
+  (allExistinTasks: Task[], schedule: BinarySchedule): Task[];
+}
+
+export const binaryScheduleToTasks = (
+  allExistinTasks: Task[],
+  schedule: BinarySchedule
+) => {
+  return schedule
+    .map((isPresent, index) =>
+      isPresent
+        ? findTaskByExecutor(allExistinTasks, numberToExecutor(index + 1))
+        : undefined
+    )
+    .filter((elem): elem is Task => !!elem);
+};
+
+function findTaskByExecutor(allExistinTasks: Task[], executor: Executor): Task {
+  const task = allExistinTasks.find((task) => task.executor === executor);
+  if (!task) {
+    throw new Error(`No task with executor ${executor}`);
+  }
+
+  return task;
+}
+
+function numberToExecutor(num: number): Executor {
+  return `M${num}`;
+}
 
 export function probability(x: number) {
   return Math.random() <= x;
